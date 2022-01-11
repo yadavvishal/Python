c33f9987dbe2205fdc405aed0945b5cad37d8ec3
yadavvishal <90525000+yadavvishal@users.noreply.github.com>
2022-01-12 00:21:09 +0530

    Add files via upload

---
+++ JoinTwoStrings.py
@@ -0,0 +1,6 @@
+def first_last(a,b):
+    print("Hello "+a+" "+b+" How are you?" )
+if __name__ == '__main__':
+    a=input("Enter the first string:")
+    b=input("Enter the second string:")
+    first_last(a,b)    
